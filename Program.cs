1d96a8c2e7f3d7706a2bd074eda71eeda0c1e938
ttetricas <estermeireles89@gmail.com>
2025-05-03 12:41:30 -0300

    nsei

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+Console.WriteLine("--Calculadora IMC--");
+
+Console.Write("Digite sua altura em metros: ");
+string alturaTexto = Console.ReadLine()?? "0";
+double altura = double.Parse(alturaTexto.Replace(',', '.'), CultureInfo.InvariantCulture);
+
+Console.Write("Digite seu peso em kg: ");
+string pesoTexto = Console.ReadLine() ?? "0";
+double peso = double.Parse(pesoTexto.Replace(',', '.'), CultureInfo.InvariantCulture);
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"\nSeu IMC é {imc:F1}");
+
+if (imc < 18.5)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Classificação: Abaixo do peso");
+}
+else if (imc < 25)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Classificação: Peso normal");
+}
+else if (imc < 30)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Classificação: Sobrepeso");
+}
+else if (imc < 35)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("Classificação: Obesidade grau I");
+}
+else if (imc < 40)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Classificação: Obesidade grau II");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Classificação: Obesidade grau III");
+}
+Console.ResetColor();
